b8ceaf348d22b74addc09ced3096419aa3ace225
Matthew <matthewdeeco@gmail.com>
2013-01-15 21:10:38 +0800

    SQL Dumps and Insert Statements

--- Create Tables.sql
+++ Create Tables.sql
@@ -1,3 +1,84 @@
+CREATE TABLE requirements (
+requirementid		SERIAL		PRIMARY KEY,
+requirementname	varchar(50),
+functionname		varchar(50)
+);
+
+CREATE TABLE persons (
+personid		SERIAL		PRIMARY KEY,
+lastname		varchar(45),
+firstname		varchar(45),
+middlename		varchar(45),
+pedigree		varchar(45)
+);
+
+CREATE TABLE instructors (
+instructorid		SERIAL	PRIMARY KEY,
+personid		integer		REFERENCES persons(personid)
+);
+
+CREATE TABLE curricula (
+curriculumid		SERIAL	PRIMARY KEY,
+curriculumname	varchar(45)	
+);
+
+CREATE TABLE students (
+studentid		SERIAL	PRIMARY KEY,
+personid		integer		REFERENCES persons(personid),
+studentno		varchar(9),
+curriculumid		integer		REFERENCES curricula(curriculumid)
+);
+
+CREATE TABLE terms (
+termid		SERIAL	PRIMARY KEY,
+name		varchar(45),
+year		varchar(9),
+sem		varchar(3)
+);
+
+CREATE TABLE grades (
+gradeid		SERIAL		PRIMARY KEY,
+gradename	varchar(4),
+gradevalue	real
+);
+
+CREATE TABLE courses (
+courseid	SERIAL		PRIMARY KEY,
+coursename	varchar(45),
+credits		real,
+domain		varchar(4),
+commtype	varchar(2)
+);
+
+CREATE TABLE classes (
+classid		SERIAL	PRIMARY KEY,
+termid		integer	REFERENCES terms(termid),
+courseid	integer		REFERENCES courses(courseid),
+section		varchar(7),
+classcode	varchar(5)
+);
+
+CREATE TABLE instructorclasses (
+instructorclassid	SERIAL	PRIMARY KEY,
+classid			integer		REFERENCES classes(classid),
+instructorid		integer		REFERENCES instructors(instructorid)
+);
+
+CREATE TABLE studentterms (
+studenttermid	SERIAL	PRIMARY KEY,
+studentid	integer		REFERENCES students(studentid),
+termid		integer	REFERENCES terms(termid),
+ineligibilities varchar,
+issettled boolean
+);
+
+CREATE TABLE studentclasses (
+studentclassid		SERIAL	PRIMARY KEY,
+studenttermid		integer		REFERENCES studentterms(studenttermid),
+classid			integer		REFERENCES classes(classid),
+gradeid			integer		REFERENCES grades(gradeid)
+);
+
 INSERT INTO curricula (curriculumname) VALUES ('new');
 INSERT INTO curricula (curriculumname) VALUES ('old');
 
@@ -23,8 +104,10 @@ INSERT INTO terms VALUES (20102, '2nd Semester 2010-2011', '2010-2011', '2nd');
 INSERT INTO terms VALUES (20103, 'Summer Semester 2010-2011', '2010-2011', 'Sum');
 INSERT INTO terms VALUES (20111, '1st Semester 2011-2012', '2011-2012', '1st');
 INSERT INTO terms VALUES (20112, '2nd Semester 2011-2012', '2011-2012', '2nd');
+INSERT INTO terms VALUES (20113, 'Summer Semester 2011-2012', '2011-2012', 'Sum');
 INSERT INTO terms VALUES (20121, '1st Semester 2012-2013', '2012-2013', '1st');
 INSERT INTO terms VALUES (20122, '2nd Semester 2012-2013', '2012-2013', '2nd');
+INSERT INTO terms VALUES (20123, 'Summer Semester 2012-2013', '2012-2013', 'Sum');
 
 INSERT INTO courses VALUES (1, 'cs 11', 3, 'MAJ', NULL);
 INSERT INTO courses VALUES (2, 'cs 12', 3, 'MAJ', NULL);
