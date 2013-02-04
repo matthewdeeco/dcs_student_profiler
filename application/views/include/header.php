fd4c95687478d78f1df5743222e85ce099ce720b
Fatima De Villa <fatima_devilla12@yahoo.com>
2013-02-04 17:55:38 +0800

    Unification with Team C's UI

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -1,102 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-   <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta name="description" content="">
-   <meta name="keywords" content="">
-   <meta name="author" content="">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
 
-   <title>UP DCS Student Profiling System</title>
+		<title>UP DCS Student Profiling System</title>
+		
+		<!-- Team C -->
+	   <link href="<?= base_url('assets/css/index.css') ?>" rel="stylesheet" type="text/css" />
+	   <link href="<?= base_url('assets/css/teamc.css') ?>" rel="stylesheet" type="text/css" />
+		<!---End Team C--->
 
-   <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
-   <link href="<?= base_url('assets/css/bootstrap-responsive.min.css') ?>" rel="stylesheet">
-   <link href="<?= base_url('assets/css/font-awesome.css') ?>" rel="stylesheet">
-   <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
-   <link href="<?= base_url('assets/css/custom.css') ?>" rel="stylesheet">
-   
-   <!-- Team C -->
-   <link href="<?= base_url('assets/css/index.css') ?>" rel="stylesheet" type="text/css" />
-   <link href="<?= base_url('assets/css/teamc.css') ?>" rel="stylesheet" type="text/css" />
-
-   
-   
-   <link href="bootstrap.min.css" rel="stylesheet">
-    <link href="../default/bootstrap-responsive.min.css" rel="stylesheet">
-    <link href="../css/docs.css" rel="stylesheet">
-
-   <script type="text/javascript">
-
-     var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-23019901-1']);
-      _gaq.push(['_setDomainName', "bootswatch.com"]);
-        _gaq.push(['_setAllowLinker', true]);
-      _gaq.push(['_trackPageview']);
-
-     (function() {
-       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-     })();
-
-   </script>
-   
-   
-
-   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
-   <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
-	<script src="<?= base_url('assets/js/custom.js') ?>"></script>
-</head>
-<body class="preview" data-spy="scroll" data-target=".subnav" data-offset="80">
-<script src="../js/bsa.js"></script>
-  <!-- Navbar
-    ================================================== -->
-
-
- 
- 
-<div class="container">
-<!-- Masthead
-================================================== -->
-
-<br/>
-<header class="jumbotron subhead" id="overview">
-  <div class="row">
-   
-	<div class="well" align="center">
-      <span>
-	  <a href="index.html"><img src="<?= base_url('assets/img/logo.png') ?>" align="middle" alt="Logo" /></a>
-	  </span>
-	  <span>
-	  <h1>UP DCS Student Profiling System</h1> 
-	  <p class="lead">Department of Computer Science, UP Diliman</p>
-	  </span>
-		</div>
-    
-
-
-  <div class="navbar">
-    <div class="navbar-inner">
-      <div class="container" style="width: auto;">
-        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </a>
-        <a class="brand active" href="#">Welcome!</a>
-        <div class="nav-collapse">
-          <ul class="nav">
-            <li><a href="#">Student Rankings</a></li>
-            <li><a href="#">Course Statistics</a></li>
-            <li><a href="#">Eligibility Checking</a></li>
-            <li><a href="<?= site_url('update_statistics/index') ?>">Update Statistics</a></li>
-			<li><a href="#">About</a></li>
-          </ul>
-        </div><!-- /.nav-collapse -->
-      </div>
-    </div><!-- /navbar-inner -->
-  </div><!-- /navbar -->
-
-  </div>
-</header>
+		<script type="text/javascript" src="<?= base_url('assets/js/jquery-1.8.0.min.js') ?>"></script>
+		<script type="text/javascript" src="<?= base_url('assets/js/jquery-1.8.3.js') ?>"></script>
+		<script type="text/javascript" src="<?= base_url('assets/js/jquery-ui-1.8.23.custom.min.js') ?>"></script>
+		<script type="text/javascript" src="<?= base_url('assets/js/jquery-ui-timepicker-addon.js') ?>"></script>
+		<script type="text/javascript" src="<?= base_url('assets/js/jquery-ui-sliderAccess.js') ?>"></script>
+		<script type="text/javascript" src="<?= base_url('assets/js/jquery.tablesorter.js') ?>"></script>
+		
+		<link href="<?= base_url('assets/css/font-awesome.css') ?>" rel="stylesheet">
+		<link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+		<link href="<?= base_url('assets/css/bootstrap-responsive.min.css') ?>" rel="stylesheet">
+		<link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
+		<link href="<?= base_url('assets/css/custom.css') ?>" rel="stylesheet">
+		<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+		<script src="<?= base_url('assets/js/custom.js') ?>"></script>
+	</head>
+	<body class="preview" data-spy="scroll" data-target=".subnav" data-offset="80">
+		<div class="container">
+		<br/>
+		<header class="jumbotron subhead" id="overview">
+			<div class="row">   
+				<div class="well" align="center">
+					<span>
+						<a href="<?= base_url('')?>"><img src="<?= base_url('images/logo.png')?>" align="middle" alt="Logo" /></a>
+					</span>
+					<span>
+						<h1>UP DCS Student Profiling System</h1>  
+						<p class="lead">Department of Computer Science, UP Diliman</p>
+					</span>
+				</div>
+				
+				<div class="navbar">
+					<div class="navbar-inner">
+						<div class="container" style="width: auto;">
+							<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</a>
+							<a class="brand" href="<?= base_url('')?>"><i class="icon-home2" style="padding:4px; padding-right:8px"></i>Welcome!</a>
+							<div class="nav-collapse">
+								<ul class="nav">
+									<li id="sr"><a href="<?= site_url('studentrankings')?>" style="height:23px;">Student Rankings</a></li>
+									<li id="cs"><a href="<?= site_url('coursestatistics')?>" style="height:23px;">Course Statistics</a></li>
+									<li id="et"><a href="<?= site_url('eligibilitytesting')?>" style="height:23px;">Eligibility Checking</a></li>
+									<li id="us"><a href="<?= site_url('update_statistics')?>" style="height:23px;">Update Statistics</a></li>
+									<li id="ab"><a href="<?= site_url('about')?>" style="height:23px;">About</a></li>
+								</ul>
+							</div><!-- /.nav-collapse -->
+						</div>
+					</div><!-- /navbar-inner -->
+			  </div><!-- /navbar -->
+			</div>
+		</header>
