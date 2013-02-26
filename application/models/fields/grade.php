7bb72a717e94eb40ebf748d483287183b2faf11c
Molen Fernando <mooolen@gmail.com>
2013-02-27 00:28:33 +0800

    Minor fix on grade parsing bug.

--- application/models/fields/grade.php
+++ application/models/fields/grade.php
@@ -11,25 +11,22 @@ class Grade extends Field {
 				$grade = "NG";
 			else
 				throw new Exception("Unexpected input in compgrade");
-		else if (is_numeric($grade))
+		else if (preg_match('/^([12](\.([27]50*|[05]0*))?)$|^([345](\.0*)?)$/', $grade))
 			$grade = number_format($grade, 2); // make into 2 decimal places
-		else if (ctype_alpha($grade))
+		else if (preg_match('/^DRP|NG|INC$/i', $grade))
 			$grade = strtoupper($grade);
-		if (preg_match('/^([12](\.([27]5|[05]0)))$|^([35](\.00))$/', $grade))
-			; // leave grade as is
-		else if (!strcmp('DRP', $grade) || !strcmp('NG', $grade))
-			; // leave grade as is
-		else if (!strcmp('4.00', $grade) || !strcmp('INC', $grade)) {
+		else
+			throw new Exception("Invalid input in grade");
+
+		if (!strcmp('4.00', $grade) || !strcmp('INC', $grade)) {
 			if (empty($compgrade))
 				; // leave grade as is
 			else if (is_numeric($compgrade)) {
 				$compgrade = number_format($compgrade, 2); // make into 2 decimal places
-				if (preg_match('/^([12](\.([27]5|[05]0)))$|^([35](\.00))$/', $compgrade))
+				if (preg_match('/^([12](\.([27]50*|[05]0*))?)$|^([35](\.0*)?)$/', $compgrade))
 					$grade = $compgrade;
 			}
 		}
-		else
-			throw new Exception("Invalid input in grade");
 		$this->values['grade'] = $grade;
 	}
 }
