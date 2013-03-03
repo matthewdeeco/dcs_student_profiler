299d7918c0a607d6aa3a15b2949d24b7fb21b827
Anna Janeri Ongcol <annajaneriongcol@gmail.com>
2013-03-03 13:59:40 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/dcs_student_profiler/';
+$config['base_url']	= 'http://localhost:8888/dcs_student_profiler/';
 
 /*
 |--------------------------------------------------------------------------
